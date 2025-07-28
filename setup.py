56ddf14b3dfbd6766085db475930aa1c2a94c2a1
shyandsy <shyandsy@gmail.com>
2025-07-28 15:10:41 +0000

    add files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pylabelme2yolo",
-    version="0.1.1",
+    version="0.1.2",
     author="shyandsy",
     author_email="shyandsy@gmail.com",
     description="Convert LabelMe annotations to YOLO format",
